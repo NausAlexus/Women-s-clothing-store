6ae374fafd75e03ee41b09e6805132943c0e7dd0
NausAlexus <naus.lexus@mail.ru>
2024-10-06 20:49:57 +0300

    media firs

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,19 +8,19 @@ function Nav() {
             <ul className="nav-container">
 
                 <li className="nav-item">
-                    <a href="https://uk.triumph.com/" className="nav-category nav-home">
+                    <a href="#" className="nav-category nav-home">
                         <span itemprop="name">Home</span>
                     </a>
                 </li>
                 <pre> / </pre>
                 <li className="nav-item">
-                    <a href="https://uk.triumph.com/loungewear-nightwear" className="nav-category nav-loungewear">
+                    <a href="#" className="nav-category nav-loungewear">
                         <span>Loungewear</span>
                     </a>
                 </li>
                 <pre> / </pre>
                 <li className="nav-item">
-                    <a href="https://uk.triumph.com/loungewear/shirts-tops-camisoles" className="nav-category nav-tops">
+                    <a href="#" className="nav-category nav-tops">
                         <span>Camisoles, vests, tops</span>
                     </a>
                 </li>
